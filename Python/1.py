2faf7b0747efb85281e8ac2c45369ca8026f971f
psweeyagoud <63531280+sweeya99@users.noreply.github.com>
2020-07-22 14:42:14 +0530

    Update 1.py

--- Python/1.py
+++ Python/1.py
@@ -1,3 +1,5 @@
+import sys
+
 def is_prime(n):
    for i in range(2, n):
       if n % i == 0:
@@ -8,7 +10,9 @@ def generate_twins(start, end):
    for i in range(start, end):
       j = i + 2
       if(is_prime(i) and is_prime(j)):
-         print("{:d} and {:d}".format(i, j))
+         f = open("myFirstFile.txt", "a")
+         print("{:d} and {:d}".format(i, j) ,file=f)
+         f.close()
 
 x = int (input ("enter the no of digits : "))
 start =(pow (10 , (x-1)) )+ 1
