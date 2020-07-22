a5a1728cdd2be32ce74a4e02bb75d6b6c19bdda7
psweeyagoud <63531280+sweeya99@users.noreply.github.com>
2020-07-22 13:12:49 +0530

    Update 4.py

--- Python/4.py
+++ Python/4.py
@@ -1,31 +1,22 @@
 import numpy as np
 from numpy.linalg import inv
 
-# = np.array([1,2,3], dtype='int32')
+#random y of type int32
 y= np.random.randint(1,5,(20,1), dtype ='int32')
 
 #print(y)
-x = np.random.normal(0,1.0 , (20,20))  #becoz normalized
+x = np.random.normal(0,1.0 , (20,20))  #we can change values mean,std deviation
 #print(x)
 
-
-
-#print(k)
 xinv = inv(x)
-#print(k)
-#print(kinv)
-
 xtrans = x.transpose()
-#print(ktrans)
-
-#xinv = inv(x)
+#print(xtrans)
 #print(xinv)
-#xtrans = x.transpose()
-#print(x)
+
 a=np.matmul(xtrans,x)
 b=np.matmul(xtrans,y)
 j=inv(a)
 theta = np.matmul(j,b)
 print(theta)
 
-#(xtrans)
+
